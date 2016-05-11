f3c2e339a635a234fa4fa65335f55fbe80b25f48
TusharBalar <balar.tushar1@gmail.com>
2016-05-11 15:53:42 +0530

    Code refactoring

--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,7 @@
-angular.module('starter.services', [])
+angular.module('yugma.services', [])
 
 .factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
   var chats = [{
     id: 0,
     name: 'Ben Sparrow',
@@ -15,17 +13,7 @@ angular.module('starter.services', [])
     lastText: 'Hey, it\'s me',
     face: 'img/max.png'
   }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
     id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
     name: 'Mike Harrington',
     lastText: 'This is wicked good ice cream.',
     face: 'img/mike.png'
