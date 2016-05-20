595a4973e2806cacfb81c05a1fffe63eb3cdb79b
TusharBalar <balar.tushar1@gmail.com>
2016-05-20 18:46:42 +0530

    Update login page styles

--- www/js/controllers/loginCtrl.js
+++ www/js/controllers/loginCtrl.js
@@ -1,100 +1,100 @@
 angular.module('yugma')
 
-.controller('LoginCtrl', function($scope, $state, $ionicPopup, $ionicPlatform, customService, authService, USER) {
+	.controller('LoginCtrl', function ($scope, $state, $ionicPopup, $ionicPlatform, customService, authService, USER) {
 
-    $ionicPlatform.registerBackButtonAction(function (event) {
+		$ionicPlatform.registerBackButtonAction(function (event) {
 
-		if ($state.current.name === "tab.dash") {
-			// remove this line to disable the exit
-			navigator.app.exitApp();
-		} else {
-			navigator.app.backHistory();
-		}
-
-    }, 100);
-
-	$scope.parentsCredentials = {
-		contact : "",
-		otp: "",
-		displayUserTextbox : true
-	};
-
-	$scope.category ={
-       state : "parents"
-   	};
-
-	$scope.loginCategory = function() {
-
-		if ($scope.category.state === "parents") {
-			$state.go("login.parents");
-		} else {
-			$state.go("login.managements");
-		}
-
-	}
-
-	$scope.isFormDisabled = function(){
-		return this.form.$invalid;
-	}
-
-	var parentsData = {};
-
-	$scope.authenticateUser = function(data) {
-
-		customService._on();
-
-		authService.getOtp(data.contact) .then(function(data) {
-
-			customService._off();
-
-			$scope.parentsCredentials = {};
-
-			if (typeof data === "object") {
-
-				$scope.parentsCredentials.displayUserTextbox = false;
-
-				parentsData = {
-					parentId: data.id,
-					parentOtp: data.otp,
-					parentName: data.name
-				};
-				
+			if ($state.current.name === "tab.dash") {
+				// remove this line to disable the exit
+				navigator.app.exitApp();
 			} else {
-				$scope.parentsCredentials.displayUserTextbox = true;
+				navigator.app.backHistory();
 			}
-			
-		});
-	};
 
-	$scope.otpVerification = function(data) {
+		}, 100);
 
-		customService._on();
+		$scope.parentsCredentials = {
+			contact: "",
+			otp: "",
+			displayUserTextbox: true
+		};
 
-		authService.verifyOtp(data.otp, parentsData) .then(function(authenticated) {
+		$scope.category = {
+			state: "parents"
+		};
 
-			customService._off();
-			$scope.parentsCredentials = {};
-			$scope.parentsCredentials.displayUserTextbox = true;
-			$scope.category.state = "parents";
-			$state.go("tab.dash", {}, {reload: true});
+		$scope.loginCategory = function () {
 
-		}, function(err) {
-
-			customService._off();
-			
-			var Data = {
-				title: "Login Failed",
-				template: "Otp doesn't match."
+			if ($scope.category.state === "parents") {
+				$state.go("login.parents");
+			} else {
+				$state.go("login.managements");
 			}
-			
-			customService._showAlert(Data) .then(function(res) {
-				if (res) {
-					$scope.parentsCredentials = {};
+
+		}
+
+		$scope.isFormDisabled = function () {
+			return this.form.$invalid;
+		}
+
+		var parentsData = {};
+
+		$scope.authenticateUser = function (data) {
+
+			customService._on();
+
+			authService.getOtp(data.contact).then(function (data) {
+
+				customService._off();
+
+				$scope.parentsCredentials = {};
+
+				if (typeof data === "object") {
+
+					$scope.parentsCredentials.displayUserTextbox = false;
+
+					parentsData = {
+						parentId: data.id,
+						parentOtp: data.otp,
+						parentName: data.name
+					};
+
+				} else {
 					$scope.parentsCredentials.displayUserTextbox = true;
 				}
-			});
 
-		});
-	}
-	
-})
+			});
+		};
+
+		$scope.otpVerification = function (data) {
+
+			customService._on();
+
+			authService.verifyOtp(data.otp, parentsData).then(function (authenticated) {
+
+				customService._off();
+				$scope.parentsCredentials = {};
+				$scope.parentsCredentials.displayUserTextbox = true;
+				$scope.category.state = "parents";
+				$state.go("tab.dash", {}, { reload: true });
+
+			}, function (err) {
+
+				customService._off();
+
+				var Data = {
+					title: "Login Failed",
+					template: "Otp doesn't match."
+				}
+
+				customService._showAlert(Data).then(function (res) {
+					if (res) {
+						$scope.parentsCredentials = {};
+						$scope.parentsCredentials.displayUserTextbox = true;
+					}
+				});
+
+			});
+		}
+
+	})
