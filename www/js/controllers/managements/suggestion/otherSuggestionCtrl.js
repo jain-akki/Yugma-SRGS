2e1b7dc9748c669578e29892868bdb4c2df3553a
Akshay Jain <jain.akshay5244@gmail.com>
2016-07-02 14:46:04 +0530

    Add otherSuggestion View in management Side

---
+++ www/js/controllers/managements/suggestion/otherSuggestionCtrl.js
@@ -0,0 +1,15 @@
+﻿(function () {
+
+    'use strict';
+
+    angular.module('yugma')
+
+    .controller('managementOtherSuggestionCtrl',
+
+    function ($http, $scope, $state, USER, managementSuggestionService, customService) {
+
+        var vm = this;
+        console.log('managementOtherSuggestionCtrl');
+    });
+
+})();
