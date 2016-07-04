f4a9549a0a36be15c018d2a158917fe486fd699b
Akshay Jain <jain.akshay5244@gmail.com>
2016-07-04 17:59:14 +0530

    Done with assignOtherSuggestion

--- www/js/controllers/managements/suggestion/assignOtherSuggestionCtrl.js
+++ www/js/controllers/managements/suggestion/assignOtherSuggestionCtrl.js
@@ -8,9 +8,48 @@
 
     function ($scope, $state, $stateParams, USER, managementSuggestionService, customService) {
 
+        console.log('assignOtherSuggestionCtrl');
+
         var vm = this;
 
-        console.log('assignOtherSuggestionCtrl');
+        var roles = USER.roles();
+        customService._on();
+
+        function getDirectorOtherAssignSuggestion() {
+            managementSuggestionService.getDirectorOtherAssignSuggestion(USER.parentId()).then(function (response) {
+                customService._off();
+                console.log("getDirectorTeacherAssignSuggestion ", response);
+                vm.allSuggestion = response;
+            });
+        }
+
+        function getAdminOtherAssignSuggestion() {
+            managementSuggestionService.getAdminOtherAssignSuggestion(USER.parentId()).then(function (response) {
+                customService._off();
+                console.log("getAdminTeacherAssignSuggestion", response);
+                vm.allSuggestion = response;
+            });
+        }
+
+        angular.forEach(roles, function (val, index) {
+
+            if (val.roleId === 2 || val.roleId === 3) {
+
+                /***** For director and principle *******/
+                getDirectorOtherAssignSuggestion();
+
+            } else {
+
+                /****** For admin, teacher and co-ordinator *******/
+                // if(_.isEmpty(val.standardIds)) { return; }
+                getAdminOtherAssignSuggestion();
+            }
+
+        });
+
+        vm.viewCmpl = function () {
+            $state.go("management.suggestion.teacherSuggestion", {}, { reload: true });
+        }
 
     });
 
