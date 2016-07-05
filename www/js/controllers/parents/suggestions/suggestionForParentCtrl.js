fa6ffdef477a40b92dea3c1b8ccc18184dafd444
Akshay Jain <jain.akshay5244@gmail.com>
2016-07-05 19:39:57 +0530

    Solve the Problem of Ion-Refresher in SuggestionForParents - Management Side

--- www/js/controllers/parents/suggestions/suggestionForParentCtrl.js
+++ www/js/controllers/parents/suggestions/suggestionForParentCtrl.js
@@ -12,13 +12,45 @@
 
         console.log('suggestionForParentCtrl');
 
+        var init = true;
+
         $scope.suggestionsByTeacher = [];
 
         suggestionService.getSuggestionByTeacher(USER.parentId()).then(function (response) {
             customService._off();
-            $scope.suggestionsByTeacher = response;
-            console.log('suggestion: ', response);
+            
+            angular.forEach(response, function (val, index) {
+
+                if (init) {
+                    $scope.lastId = val.suggestionId;
+                    init = false;
+                }
+                $scope.suggestionsByTeacher.push(val);
+                console.log('val ', val);
+                console.log('$scope.lastId ', $scope.lastId);
+            });
+
         });
+
+        $scope.doRefresh = function () {
+            init = true;
+            suggestionService.getSuggestionByTeacher(USER.parentId()).then(function (response) {
+
+                angular.forEach(response, function (val, index) {
+                    if (init) {
+                        $scope.newlastId = val.suggestionId;
+                        init = false;
+                    }
+                    if (val.suggestionId > $scope.lastId) {
+                        $scope.suggestionsByTeacher.push(val);
+                    }
+
+                });
+                $scope.lastId = $scope.newlastId;
+                console.log('$scope.newlastId ', $scope.lastId);
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        };
 
     })
 
