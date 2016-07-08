8191938a0da41086559a145f7b65c1c08df7f641
Akshay Jain <jain.akshay5244@gmail.com>
2016-07-08 09:43:30 +0530

    Done with Poll - Parents Side
    Left with background problem

--- www/js/controllers/parents/feedback/pollCtrl.js
+++ www/js/controllers/parents/feedback/pollCtrl.js
@@ -1,10 +1,60 @@
-﻿(function(){
+﻿(function () {
+
+    'use strict';
+
     angular.module('yugma')
 
-      .controller('pollCtrl', function () {
+      .controller('pollCtrl', function ($scope, USER, parentPollService) {
 
           console.log('pollCtrl');
+
           var vm = this;
+
+          vm.pollData = [];
+          var childStandardIds = [];
+          var user = USER.parentId();
+          var childs = USER.parentChilds();
+          
+          for (i = 0; i < childs.length; i++) {
+              childStandardIds.push(childs[i].standardId);
+          }
+
+          parentPollService.fetchPollsForParent(childStandardIds, user).then(function (response) {
+              angular.forEach(response, function (val) {
+                  if(val.done === false){
+                    vm.pollData.push(val);
+                  }
+              });
+          });
+
+          vm.selectAnswer = function (answer, poll) {
+
+              var data = {
+                  pollId: poll.pollId,
+                  parentId: user,
+                  resultId: answer
+              };
+              
+              parentPollService.saveParentData(data).then(function (response) {
+                  angular.forEach(vm.pollData, function (val, index) {
+                      if (poll.pollId == val.pollId) {
+                          vm.pollData.splice(index, 1);
+                      }
+                  })
+              });
+
+          }
+
+          $scope.doRefresh = function () {
+              parentPollService.fetchPollsForParent(childStandardIds, user).then(function (response) {
+                  angular.forEach(response, function (val) {
+                      if (val.done === false && val.pollId > vm.pollData[0].pollId) {
+                          vm.pollData.push(val);
+                      }
+                  });
+                  $scope.$broadcast('scroll.refreshComplete');
+              });
+          }
 
       });
 })();
---
+++ www/js/factory/parents/feedback/pollService.js
@@ -0,0 +1,49 @@
+﻿(function () {
+
+    "use strict";
+
+    angular.module("yugma")
+
+    .factory('parentPollService', function ($http, $q, baseUrl) {
+
+        var fetchPollsForParent = function (childStandardIds, user) {
+            
+            var deferred = $q.defer();
+            console.log('childStandardIds ', childStandardIds, 'user ', user)
+            $http({
+                method: "GET",
+                contentType: "application/json",
+                url: baseUrl + "/fetch-poll-for-parent/" + childStandardIds + "/" + user
+            }).success(function (response) {
+                deferred.resolve(response);
+            }).error(function (response) {
+                deferred.reject(response);
+            });
+
+            return deferred.promise;
+        }
+
+        var saveParentData = function (pollAnswer) {
+
+            var deferred = $q.defer();
+            
+            $http({
+                method: "POST",
+                data: pollAnswer,
+                contentType: "application/json",
+                url: baseUrl + "/save-poll-result"
+            }).success(function (response) {
+                deferred.resolve(response);
+            }).error(function (response) {
+                deferred.reject(response);
+            });
+
+            return deferred.promise;
+        }
+
+        return {
+            fetchPollsForParent: fetchPollsForParent,
+            saveParentData: saveParentData
+        }
+    })
+})();
